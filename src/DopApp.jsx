82765f77446d6263cff2894d6da2fde6cef0c053
monika0019 <jbmonika019@gmail.com>
2023-03-31 17:23:28 +0200

    add useEffect

--- src/DopApp.jsx
+++ src/DopApp.jsx
@@ -1,32 +1,142 @@
-import './App.css';
+import "./App.css";
 import React, { useEffect, useState, Component } from "react";
 import Messages from "./Messages";
 import Input from "./Input";
-import {setUsername } from './Landing'
-import Landing from './Landing';
- 
+import { setUsername } from "./Landing";
+import Landing from "./Landing";
+
 function randomName() {
   const adjectives = [
-    "autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark",
-    "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter",
-    "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue",
-    "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long",
-    "late", "lingering", "bold", "little", "morning", "muddy", "old", "red",
-    "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering",
-    "withered", "wild", "black", "young", "holy", "solitary", "fragrant",
-    "aged", "snowy", "proud", "floral", "restless", "divine", "polished",
-    "ancient", "purple", "lively", "nameless"
+    "autumn",
+    "hidden",
+    "bitter",
+    "misty",
+    "silent",
+    "empty",
+    "dry",
+    "dark",
+    "summer",
+    "icy",
+    "delicate",
+    "quiet",
+    "white",
+    "cool",
+    "spring",
+    "winter",
+    "patient",
+    "twilight",
+    "dawn",
+    "crimson",
+    "wispy",
+    "weathered",
+    "blue",
+    "billowing",
+    "broken",
+    "cold",
+    "damp",
+    "falling",
+    "frosty",
+    "green",
+    "long",
+    "late",
+    "lingering",
+    "bold",
+    "little",
+    "morning",
+    "muddy",
+    "old",
+    "red",
+    "rough",
+    "still",
+    "small",
+    "sparkling",
+    "throbbing",
+    "shy",
+    "wandering",
+    "withered",
+    "wild",
+    "black",
+    "young",
+    "holy",
+    "solitary",
+    "fragrant",
+    "aged",
+    "snowy",
+    "proud",
+    "floral",
+    "restless",
+    "divine",
+    "polished",
+    "ancient",
+    "purple",
+    "lively",
+    "nameless",
   ];
   const nouns = [
-    "waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning",
-    "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter",
-    "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook",
-    "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly",
-    "feather", "grass", "haze", "mountain", "night", "pond", "darkness",
-    "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder",
-    "violet", "water", "wildflower", "wave", "water", "resonance", "sun",
-    "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog",
-    "smoke", "star"
+    "waterfall",
+    "river",
+    "breeze",
+    "moon",
+    "rain",
+    "wind",
+    "sea",
+    "morning",
+    "snow",
+    "lake",
+    "sunset",
+    "pine",
+    "shadow",
+    "leaf",
+    "dawn",
+    "glitter",
+    "forest",
+    "hill",
+    "cloud",
+    "meadow",
+    "sun",
+    "glade",
+    "bird",
+    "brook",
+    "butterfly",
+    "bush",
+    "dew",
+    "dust",
+    "field",
+    "fire",
+    "flower",
+    "firefly",
+    "feather",
+    "grass",
+    "haze",
+    "mountain",
+    "night",
+    "pond",
+    "darkness",
+    "snowflake",
+    "silence",
+    "sound",
+    "sky",
+    "shape",
+    "surf",
+    "thunder",
+    "violet",
+    "water",
+    "wildflower",
+    "wave",
+    "water",
+    "resonance",
+    "sun",
+    "wood",
+    "dream",
+    "cherry",
+    "tree",
+    "fog",
+    "frost",
+    "voice",
+    "paper",
+    "frog",
+    "smoke",
+    "star",
   ];
   const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
   const noun = nouns[Math.floor(Math.random() * nouns.length)];
@@ -34,7 +144,7 @@ function randomName() {
 }
 
 function randomColor() {
-  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+  return "#" + Math.floor(Math.random() * 0xffffff).toString(16);
 }
 
 /*export const NextPage = ({ nickname }) => {
@@ -45,57 +155,60 @@ function randomColor() {
   );
   };*/
 
-function DopApp () {
-  const [messages, setMessages]= useState([]);
-  const [member, setMember]= useState({
+function DopApp() {
+  const [messages, setMessages] = useState([]);
+  const [currentMember, setCurrentMember] = useState({
     username: randomName(),
     avatar: randomColor(),
-    id: ''
+    id: "",
   });
+
+  const drone = new window.Scaledrone("OKoLR1ZgZTNHMeUZ", {
+    data: currentMember,
+  });
+
   
-    const drone = new window.Scaledrone("ExTsKSbEDHLDRpqH", {
-      data: member
-    });
-    drone.on('open', error => {
+  useEffect(() => {
+    drone.on("open", (error) => {
       if (error) {
-        return console.error(error);
+        return console.log(error);
       }
-      const member = {...member};
+
+      console.log("openning connection");
+      console.log("drone", drone);
+      const member = { ...currentMember };
       member.id = drone.clientId;
-      setMember(member);
+      setCurrentMember(member);
     });
     const room = drone.subscribe("observable-room");
-    room.on('data', (data, member) => {
+    room.on("data", (messages, member) => {
       const newMessage = messages;
-      messages.push({member, text: data});
-      setMessages(prevMessages => [...prevMessages, newMessage]);
+      setMessages((prevMessages) => [...prevMessages, newMessage]);
     });
+    drone.on("error", (error) => {
+      console.error("Error with connection:", error);
+    });
+    drone.on("close", (event) => {
+      console.log("Connection closed:", event);
+    });
+  });
 
-
-    const onSendMessage = (message) => {
-      drone.publish({
-        room: "observable-room",
-        message
-      });
-    }
-
-    
-
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h1>DopApp</h1>
-        </div>
-        <Messages
-          messages={messages}
-          currentMember={member}
-        />
-        <Input
-          onSendMessage={onSendMessage}
-        />
+  const onSendMessage = (message) => {
+    drone.publish({
+      room: "observable-room",
+      message,
+    });
+  };
+  
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h1>DopApp</h1>
       </div>
-    );
-  }
-
+      <Messages messages={messages} currentMember={currentMember} />
+      <Input onSendMessage={onSendMessage} />
+    </div>
+  );
+}
 
 export default DopApp;
